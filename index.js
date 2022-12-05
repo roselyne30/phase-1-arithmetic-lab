098615808cbe30ac5c12360255075aab23b1b5b0
roselynelutomia <roselyne.lutomia@student.moringaschool.com>
2022-12-05 19:34:53 +0300

    <type a commit message>

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1*num2
+
+let random = Math.floor(Math.random()*10)+1;
+
+let num3 = 24
+let num4 = 5
+const mod = num3%num4
+
+
+let max = Math.max(2,20,6,12,16);
+
+
+
+
+
+
+
+
+
+
